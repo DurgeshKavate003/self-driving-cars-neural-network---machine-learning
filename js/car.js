f730af416fe7495e86a285d1ed4cb35e2292a059
DurgeshKavate003 <durgesh.kavate@outlook.com>
2023-05-19 10:55:07 +0530

    defining the road

--- js/car.js
+++ js/car.js
@@ -15,7 +15,10 @@ class Car {
     }
 
     update () {
-
+        this.#move();
+    }
+    
+    #move() {
         // FORWARD & REVERSE MOVEMENT
         if (this.controls.forward) {
             this.speed += this.acceleration;
@@ -24,29 +27,29 @@ class Car {
         if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
-
+    
         if (this.speed > this.maxSpeed) [
             this.speed = this.maxSpeed
         ]
-
+    
         if (this.speed <- this.maxSpeed / 2) {
             this.speed =- this.maxSpeed / 2;
         }
-
+    
         if (this.speed > 0) {
             this.speed -= this.friction;
         }
-
+    
         if (this.speed < 0) {
             this.speed += this.friction;
         }
-
+    
         if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-
+    
         // RIGHT & LEFT MOVEMENT
-
+    
         if (this.speed != 0) {
             const flip = this.speed > 0 ? 1 : -1;
         
@@ -59,11 +62,12 @@ class Car {
             }
         
         }
-
-
+    
+    
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
         // this.y -= this.speed
+
     }
 
     draw (ctx) {
